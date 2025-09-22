76612c6e86955238bcb55ddd4348d35ebe5d02d3
nn19991221 <nn19991221@gmail.com>
2025-09-21 19:55:49 -0700

    Finish Day2 Homework: dynamic partners section + JS

--- script.js
+++ script.js
@@ -35,3 +35,22 @@ function currentTime() {
 // 启动时钟
 currentTime();
 setInterval(currentTime, 1000);
+const partners = [
+  { src: "partners/partner-bustour.png", alt: "Partner - Bus Tour" },
+  { src: "partners/partner-cabinrental.png", alt: "Partner - Cabin Rental" },
+  { src: "partners/partner-campingadv.png", alt: "Partner - Camping Adventure" },
+  { src: "partners/partner-collegetours.png", alt: "Partner - College Tours" },
+  { src: "partners/partner-rentalbike.png", alt: "Partner - Rental Bike" },
+  { src: "partners/partner-tourgroup.png", alt: "Partner - Tour Group" }
+];
+
+const list = document.getElementById("partners");
+partners.forEach(p => {
+  const li = document.createElement("li");
+  li.className = "partner";
+  const img = document.createElement("img");
+  img.src = p.src;
+  img.alt = p.alt;
+  li.appendChild(img);
+  list.appendChild(li);
+});
